a8bffad67bd257b9140664e09703d214fc2bf8ed
EE201201 <ios.devpino@platb.ai>
2020-12-23 15:44:23 +0900

    1. Setting App Center & Add Secret Key

--- sampleapp-ios-swift/AppDelegate.swift
+++ sampleapp-ios-swift/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Enabled App Center SDK verbose logs
         MSAppCenter.setLogLevel(.verbose)
         
-        MSAppCenter.start("<APP SECRET HERE>", withServices: [
+        MSAppCenter.start("c4be117c-0f08-4542-af07-00f34bb53d3b", withServices: [
             MSAnalytics.self,
             MSCrashes.self,
             MSPush.self,
